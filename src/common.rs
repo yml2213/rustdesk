d480e6271f867b6b07f15185a1848973d4a28f61
yml2213 <a2213290201@gmail.com>
2025-03-20 09:58:31 +0800

    api服务器要内置

--- src/common.rs
+++ src/common.rs
@@ -940,7 +940,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://nas.221329.xyz:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
